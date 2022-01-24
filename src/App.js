f83e7c4e104d567e6b7d0dc47f2d03c26338eebc
PolinaStamenova <polina.stamenova@gmail.com>
2022-01-24 20:40:18 +0200

    Add mini-css to the project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import "./App.css";
-import Nav from "./components/Nav";
-import Home from "./components/Home";
-import Projects from "./components/Projects";
-import About from "./components/About";
-import Contact from "./components/Contact";
+import './App.css';
+import Nav from './components/Nav';
+import Home from './components/Home';
+import Projects from './components/Projects';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <div>
-        <div className='border'>
+        <div className="border">
           <Nav />
           <Home />
           <Projects />
